0b57c2419d40fc12e61f98c34e3cc0c6e998af73
TheAbadon <baoscb11@gmail.com>
2026-06-04 21:22:35 +0700

    Update avatar URL

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const defaultData = {
         { key: 'email', label: 'Email', value: 'baoscb11@gmail.com' },
         { key: 'phone', label: 'Số điện thoại', value: '0364 355 610' },
         { key: 'location', label: 'Địa điểm', value: 'Quảng Ngãi' },
-        { key: 'avatar', label: 'Ảnh đại diện', value: 'https://scontent.fsgn2-10.fna.fbcdn.net/v/t39.30808-6/484850184_678932217922441_3977152377095809161_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGwAW5M1QgT7BrUd2IBDnHBoRyRV5vSC6-hHJFXm9ILr8zCb61_C1dIE0zWZIQFErK-suhwf3op_pX9ARFl_yMt&_nc_ohc=_Zl8OreUIGsQ7kNvwEtSxyO&_nc_oc=Ado516Qp7zqrw03DXDIzeS9HFKrs4x46vSPfFRuZ9N9f94S0Ba3HXgRXNOic-IrKSE13iOEp53XtVnlBiAN9I-lN&_nc_zt=23&_nc_ht=scontent.fsgn2-10.fna&_nc_gid=yhl1TOSMkWe9qX067Q0PGQ&_nc_ss=7b2a8&oh=00_Af6Bgplcg5fJJZkBAH-veHLaTrlFQlC0d8wNndsfs6BWcw&oe=6A163C0A' },
+        { key: 'avatar', label: 'Ảnh đại diện', value: 'https://scontent.fsgn2-10.fna.fbcdn.net/v/t39.30808-6/484850184_678932217922441_3977152377095809161_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGwAW5M1QgT7BrUd2IBDnHBoRyRV5vSC6-hHJFXm9ILr8zCb61_C1dIE0zWZIQFErK-suhwf3op_pX9ARFl_yMt&_nc_ohc=LiTroYtXzjcQ7kNvwGx5Hcz&_nc_oc=AdoqfhycR0eNO1uF_i60w-oik_7Zt9Ib0G-p38_eJBAQDT8iihFTMz8rCAJWunYZtCOr5PZoztY2S480AvlheKS9&_nc_zt=23&_nc_ht=scontent.fsgn2-10.fna&_nc_gid=bJMxaIop3x37p23Fqy8s3Q&_nc_ss=7b2a8&oh=00_Af-3RfXqMi60mkw6S5wgNpbkXb2YLWSuXar3Bc_mxj_yHA&oe=6A275F8A' },
         { key: 'github', label: 'GitHub', value: 'https://github.com/TheAbadon' },
         { key: 'facebook', label: 'Facebook', value: 'https://www.facebook.com/nguyen.uc.bao.48593' },
         { key: 'countdownEvent', label: 'Tên sự kiện đếm ngược', value: 'Thi THPT quốc gia' },
